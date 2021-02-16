277dccfbc33b6e879d413fd0fb51ae4b64a5d336
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-02-16 23:45:02 +0900

    init blank project

---
+++ TodoTDD/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TodoTDD
+//
+//  Created by bht on 2021/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Hello Project TodoTDD with Swift")
+    }
+
+
+}
+
